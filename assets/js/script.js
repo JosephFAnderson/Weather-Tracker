2326a6f293a76412b30fbc3a917f118ecbe8a252
Joseph A <joseph_anderson11@yahoo.com>
2022-09-22 18:21:35 -0400

    HTML skeleton mostly done

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Check weather with lon + lat
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude=hourly,minutely&units=imperial&appid=b04d17a1b02b4977fbc6a49af4c0395d'
+// fetch(requestUrl)
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json()
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+
+//   Check city names and get lon + lat
+//   var newRequestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=atlanta&appid=b04d17a1b02b4977fbc6a49af4c0395d'
+//   fetch(newRequestUrl)
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json()
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
