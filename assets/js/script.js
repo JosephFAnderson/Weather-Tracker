822adb92b7a4b2eb136922cb010a3e6d67acfb74
Joseph A <joseph_anderson11@yahoo.com>
2022-09-23 10:05:38 -0400

    new request url and comment for how to do forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 // Check weather with lon + lat
-// var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude=hourly,minutely&units=imperial&appid=b04d17a1b02b4977fbc6a49af4c0395d'
-// fetch(requestUrl)
-//   .then(function (response) {
-//     console.log(response);
-//     return response.json()
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
+var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=seoul&exclude=hourly,minutely&units=imperial&appid=b04d17a1b02b4977fbc6a49af4c0395d'
+fetch(requestUrl)
+  .then(function (response) {
+    console.log(response);
+    return response.json()
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 
-//   Check city names and get lon + lat
-//   var newRequestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=atlanta&appid=b04d17a1b02b4977fbc6a49af4c0395d'
-//   fetch(newRequestUrl)
-//   .then(function (response) {
-//     console.log(response);
-//     return response.json()
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
+// Index for for 5 day forcast every 8: (var i = 1, i <= 5, i++) day = (i*8) -1
+var forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=seoul&units=imperial&appid=b04d17a1b02b4977fbc6a49af4c0395d'
+fetch(forecastUrl)
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then( function(data) {
+    console.log(data);
+  });
